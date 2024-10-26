b4898c244e9567270cb8426522089326755b2760
FarzamAhmed48 <farzamahmed48@gmail.com>
2024-10-26 10:42:54 +0500

    Changes after connecting MongoAtlas

--- backend/utils/dbConnection.js
+++ backend/utils/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const dbConnect =async()=>{
     try {
-        mongoose.connect(process.env.MONGO_URI)
+        mongoose.connect(process.env.MONGO_LINK)
         console.log("MongoDB Connected successfully")
     } catch (error) {
         console.log(error)   
